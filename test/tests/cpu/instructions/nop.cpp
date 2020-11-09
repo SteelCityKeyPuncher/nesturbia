ecd7d57a184ff2dd38e79d94c5267f44067295ba
SteelCityKeyPuncher <SteelCityKeyPuncher@gmail.com>
2020-11-09 14:33:36 -0500

    Added a test for the NOP instruction.

---
+++ test/tests/cpu/instructions/nop.cpp
@@ -0,0 +1,23 @@
+#include <array>
+#include <cstdint>
+
+#include "catch2/catch_all.hpp"
+
+#include "nesturbia/cpu.hpp"
+
+TEST_CASE("Cpu_Instructions_NOP", "[cpu]") {
+  std::array<uint8_t, 0x10000> memory;
+
+  auto read = [&memory](uint16_t address) { return memory.at(address); };
+  auto write = [&memory](uint16_t address, uint8_t value) { memory.at(address) = value; };
+
+  nesturbia::Cpu cpu(read, write);
+
+  // NOP
+  memory[0x00] = 0xea;
+
+  cpu.Power();
+  cpu.executeInstruction();
+
+  CHECK(cpu.cycles == 7 + 2);
+}
